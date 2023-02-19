ba1187f83b8939e4ca03d8adb95b20fb39443416
joenvihe <jorge.vicente@rimac.com.pe>
2023-02-19 02:05:17 -0500

    change my web

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Layout, Menu } from 'antd';
+import { Link } from 'react-router-dom';
+
 import './App.css';
+
+const { Header, Content, Footer } = Layout;
+
+function NavigationBar() {
+  return (
+    <Header>
+      <Menu theme="dark" mode="horizontal">
+        <Menu.Item key="home">
+          <Link to="/">Home</Link>
+        </Menu.Item>
+        <Menu.Item key="projects">
+          <Link to="/projects">Projects</Link>
+        </Menu.Item>
+        <Menu.Item key="blog">
+          <Link to="/blog">Blog</Link>
+        </Menu.Item>
+      </Menu>
+    </Header>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout className="layout">
+      <NavigationBar />
+      <Content style={{ padding: '0 50px' }}>
+        <div className="site-layout-content">Hello World</div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>
+        Personal Webpage ©{new Date().getFullYear()} Created by You
+      </Footer>
+    </Layout>
   );
 }
 
 export default App;
+
